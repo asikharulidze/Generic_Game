3120c550141b79ee9bfc655f9bd13ef60b2cc56e
asikharulidze <ana.sixarulidze@gmail.com>
2024-11-18 15:23:42 +0400

    edited

--- Generic_Game/equipment.cs
+++ Generic_Game/equipment.cs
@@ -5,4 +5,11 @@ public class Equipment
     public string Tools { get; set; }
     public string Armor { get; set; }
     public string Elixirs { get; set; }
+    
+    public Equipment(string tools, string armor, string elixirs)
+    {
+        Tools = tools;
+        Armor = armor;
+        Elixirs = elixirs;
+    }
 }
